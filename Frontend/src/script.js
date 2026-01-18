8ef87de747760f51ca6072fe9acc6e81e05b6807
TheCodedHuman <mokshmahawar05@gmail.com>
2026-01-18 19:43:04 +0530

    Update editor functionality; replace form submission with button click event and enhance styling

--- Frontend/src/script.js
+++ Frontend/src/script.js
@@ -39,3 +39,32 @@ editor.addEventListener("paste", () => setTimeout(updateGutter, 0));
 // initial render
 updateGutter();         // first time rendering
 
+
+// magic-btn functionality
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("magic-btn");
+    const editor = document.getElementById("editor-text");
+    const output = document.getElementById("formatted-code");
+
+    button.addEventListener("click", async () => {
+        const rawCode = editor.value;
+
+        try {
+            const response = await fetch("http://localhost:8000/format", {
+                method: "POST",w
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: new URLSearchParams({ raw_code: rawCode })
+            });
+
+            const data = await response.json();
+            output.textContent = data.formatted_code;
+        } catch (error) {
+            console.error("Error:", error);
+            output.textContent = "Something went wrong :(";
+        }
+
+    })
+});
+
