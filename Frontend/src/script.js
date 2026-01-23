64854dfc25c032b694d3c18f8145d80a361d1739
TheCodedHuman <mokshmahawar05@gmail.com>
2026-01-23 21:04:00 +0530

    Refactor HTML and CSS for improved styling; enhance error handling in JavaScript

--- Frontend/src/script.js
+++ Frontend/src/script.js
@@ -29,6 +29,10 @@ function updateGutter() {
     let s = "";
     for (let i = 1; i <= lines; i++) s += i + "\n";
     gutter.value = s;
+
+    // Digit Calculation
+    const digits = lines.toString().length;
+    gutter.style.width = `calc(${digits}ch + 1rem)`;
 }
 
 // Update on input and sync scrolling
@@ -44,7 +48,8 @@ updateGutter();         // first time rendering
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("magic-btn");
     const editor = document.getElementById("editor-text");
-    const output = document.getElementById("formatted-output");
+    const output = document.getElementById("formatted-output");         // tree output
+    const outputContainer = document.getElementById("formatted-code");  // container
 
     button.addEventListener("click", async () => {
         const rawCode = editor.value;
@@ -58,15 +63,27 @@ document.addEventListener("DOMContentLoaded", () => {
                 },
                 body: new URLSearchParams({ raw_code: rawCode })
             });
+            if (!response.ok) throw new Error(`Server Error: ${response.status}`);
 
             const data = await response.json();
             window.data = data;
             render()
+            outputContainer.style.overflow = "auto";            // Restore scrollbar just in case it was hidden by a previous error
+            outputContainer.style.padding = "1rem 0";           // Restore breathing room for code
+
         } catch (error) {
-            console.error("Error:", error);
-            output.textContent = "Something went wrong :( (Fronend Side)";
+            console.error("Error:", error);                 // Inject HTML with a specific class we will style in CSS
+            outputContainer.style.overflow = "hidden";      // Hide scrollbar so the error message 
+            outputContainer.style.padding = "0";            // Remove padding so it centers perfectly
+            output.innerHTML = `<div class="error-msg">
+                    <i class="fa-solid fa-triangle-exclamation"></i>
+                    <h3>Something went wrong :(</h3>
+                    <p>Frontend Side</p>
+                    <small>${error.message}</small>
+                </div>`;
         }
 
     })
 });
+
 
